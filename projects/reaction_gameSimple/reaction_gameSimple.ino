2682483b0f9d35ac9c4868ed31513bf20b974b89
LeluchVIBritannia <alvinshahzer0123@gmail.com>
2026-04-25 14:24:08 +0545

    Reaction game

--- projects/reaction_gameSimple/reaction_gameSimple.ino
+++ projects/reaction_gameSimple/reaction_gameSimple.ino
@@ -1,16 +1,16 @@
 int startLed = 13;
 int inputPlater1 = 12;
 int inputPlater2 = 8;
-int player1Led;
-int player2led;
-int delayTime = random(100 , 2001);
+int player1Led = 10;
+int player2led = 11;
+int delayTime;
 
 void setup() 
 {
   //LED PIN
   pinMode(startLed , OUTPUT);
   pinMode(player1Led , OUTPUT);
-  pinMode(playe21Led , OUTPUT);
+  pinMode(player2led , OUTPUT);
   //Input pins
   pinMode(inputPlater1 , INPUT_PULLUP);
   pinMode(inputPlater2  , INPUT_PULLUP);
@@ -18,9 +18,10 @@ void setup()
   //For starting signal
   Serial.begin(9600);
   randomSeed(analogRead(A0));
-
+  delayTime = random(100 , 3001);
   delay(delayTime);
   digitalWrite(startLed , HIGH);
+  Serial.println(delayTime);
 }
 
 void loop()
